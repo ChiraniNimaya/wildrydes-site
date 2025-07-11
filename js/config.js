b2b9c70f362d11161e4aefdf904777da81f2e685
Chirani Rajapaksha <106186166+ChiraniNimaya@users.noreply.github.com>
2025-07-11 22:00:03 +0530

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,7 +1,7 @@
 window._config = {
     cognito: {
         userPoolId: 'eu-north-1_XyTgDpD1f', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '2pe0mql2qhl22ci4tmqi3m9otu', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        userPoolClientId: '2m7c57sne78eiqcunvu0gn7926', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
         region: 'eu-north-1' // e.g. us-east-2
     },
     api: {
